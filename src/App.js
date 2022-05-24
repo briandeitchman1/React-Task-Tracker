6b753c4df25e7a7df7a88575282f75dcce0e588d
Brian Deitchman <briandeitchman@gmail.com>
2022-05-24 16:48:00 -0700

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "dentist",
+      day: "Feb 5th at 2 ",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "shopping",
+      day: "Feb 5th at 2 ",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "pick up kids",
+      day: "Feb 5th at 2 ",
+      reminder: true,
+    }
+  ])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }

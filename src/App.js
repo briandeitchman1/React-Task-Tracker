e2240e825d70f5f08e09e477fb446de61ed2bfaa
Brian Deitchman <briandeitchman@gmail.com>
2022-05-24 21:46:02 -0700

    Can now delete tasks

--- src/App.js
+++ src/App.js
@@ -23,10 +23,21 @@ function App() {
       reminder: true,
     }
   ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => {
+      return task.id !== id
+    }))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+        : "No Task to show"
+      }
     </div>
   );
 }

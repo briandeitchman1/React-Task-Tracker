d95b073d0fca0d2908799b6f29382a19efb329c3
Brian Deitchman <briandeitchman@gmail.com>
2022-05-25 18:04:11 -0700

    new Tasks can now be added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react"
 
 function App() {
@@ -23,6 +24,13 @@ function App() {
       reminder: true,
     }
   ])
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    console.log(id);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -42,6 +50,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ?
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         : "No Task to show"
